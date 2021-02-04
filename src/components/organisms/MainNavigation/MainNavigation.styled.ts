da03f87cd3989d89428818b1b419e26aaef43375
kjwook7522 <kjwook7522@gmail.com>
2021-02-04 11:17:44 +0900

    Feat: create MainNavigation component

--- src/components/organisms/MainNavigation/MainNavigation.styled.ts
+++ src/components/organisms/MainNavigation/MainNavigation.styled.ts
@@ -9,3 +9,54 @@ export const StyledNavigation = styled.nav`
   left: 0;
   background-color: #f6f6f6;
 `;
+
+export const StyledNavigationMenu = styled.div`
+  width: 100%;
+  padding: 1.5rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #303030;
+  color: #fff;
+  cursor: pointer;
+
+  &.active {
+    background-color: #444;
+  }
+`;
+
+export const StyledNavigationSubMenu = styled.div`
+  width: 100%;
+  max-height: 0px;
+  padding: 0 2rem;
+  overflow: hidden;
+  background-color: #303030;
+  color: #fff;
+  transition: max-height 0.5s, padding 0.5s;
+
+  &.active {
+    max-height: 150px;
+    padding: 1rem 2rem;
+    background-color: #444;
+  }
+
+  @media (max-width: 1025px) {
+  }
+
+  ul {
+    li {
+      margin: 0.3rem 0;
+      padding: 0.3rem 0;
+
+      a {
+        color: #fff;
+        text-decoration: none;
+      }
+    }
+
+    li::before {
+      content: '-';
+      margin-right: 0.4rem;
+    }
+  }
+`;
--- src/components/organisms/MainNavigation/MainNavigation.tsx
+++ src/components/organisms/MainNavigation/MainNavigation.tsx
@@ -1,7 +1,99 @@
-import { StyledNavigation } from './MainNavigation.styled';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { StyledNavigation, StyledNavigationMenu, StyledNavigationSubMenu } from './MainNavigation.styled';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 const Navigation: React.FC = () => {
-  return <StyledNavigation></StyledNavigation>;
+  // ** ActiveTab ** //
+  // 0. Null         //
+  // 1. 내정보         //
+  // 2. 컨설팅         //
+  // 3. 이용 안내      //
+  const [activeTab, setActivceTab] = useState(0);
+
+  const handleMenu = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const { tab } = event.currentTarget.dataset;
+    switch (tab) {
+      case 'tab1':
+        setActivceTab(1);
+        if (activeTab === 1) {
+          setActivceTab(0);
+        }
+        return;
+      case 'tab2':
+        setActivceTab(2);
+        if (activeTab === 2) {
+          setActivceTab(0);
+        }
+        return;
+      case 'tab3':
+        setActivceTab(3);
+        if (activeTab === 3) {
+          setActivceTab(0);
+        }
+        return;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <StyledNavigation>
+      <StyledNavigationMenu data-tab="tab1" className={activeTab === 1 ? 'active' : undefined} onClick={handleMenu}>
+        <h3>김동빈</h3>
+        {activeTab === 1 ? <FiChevronUp /> : <FiChevronDown />}
+      </StyledNavigationMenu>
+      <StyledNavigationSubMenu className={activeTab === 1 ? 'active' : undefined}>
+        <ul>
+          <li>
+            <Link to="#">개인 정보 수정</Link>
+          </li>
+          <li>
+            <Link to="#">일정 관리</Link>
+          </li>
+          <li>
+            <Link to="#">결제 관리</Link>
+          </li>
+        </ul>
+      </StyledNavigationSubMenu>
+
+      <StyledNavigationMenu data-tab="tab2" className={activeTab === 2 ? 'active' : undefined} onClick={handleMenu}>
+        <h3>컨설팅 코스</h3>
+        {activeTab === 2 ? <FiChevronUp /> : <FiChevronDown />}
+      </StyledNavigationMenu>
+      <StyledNavigationSubMenu className={activeTab === 2 ? 'active' : undefined}>
+        <ul>
+          <li>
+            <Link to="#">개인 정보 수정</Link>
+          </li>
+          <li>
+            <Link to="#">일정 관리</Link>
+          </li>
+          <li>
+            <Link to="#">결제 관리</Link>
+          </li>
+        </ul>
+      </StyledNavigationSubMenu>
+
+      <StyledNavigationMenu data-tab="tab3" className={activeTab === 3 ? 'active' : undefined} onClick={handleMenu}>
+        <h3>이용 안내</h3>
+        {activeTab === 3 ? <FiChevronUp /> : <FiChevronDown />}
+      </StyledNavigationMenu>
+      <StyledNavigationSubMenu className={activeTab === 3 ? 'active' : undefined}>
+        <ul>
+          <li>
+            <Link to="#">개인 정보 수정</Link>
+          </li>
+          <li>
+            <Link to="#">일정 관리</Link>
+          </li>
+          <li>
+            <Link to="#">결제 관리</Link>
+          </li>
+        </ul>
+      </StyledNavigationSubMenu>
+    </StyledNavigation>
+  );
 };
 
 export default Navigation;
