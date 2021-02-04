9307d24ba33d774825c8ed21605971398217f123
kjwook7522 <kjwook7522@gmail.com>
2021-02-04 15:48:16 +0900

    Feat: create quicknavigation component

--- src/components/organisms/QuickNavigation/QuickNavigation.styled.ts
+++ src/components/organisms/QuickNavigation/QuickNavigation.styled.ts
@@ -4,8 +4,141 @@ import styled from 'styled-components';
 export const StyledNavigation = styled.nav`
   width: ${QUICK_NAVIGATION_WIDTH}px;
   height: calc(100vh - ${HEADER_HEIGHT}px);
+  padding: 0 1rem;
   position: fixed;
   top: ${HEADER_HEIGHT}px;
   right: 0;
   background-color: #f6f6f6;
+  overflow: scroll;
+
+  /* for hide scroll bar */
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const StyledCalendar = styled.div`
+  margin: 1.5rem 0;
+
+  h1 {
+    margin-bottom: 0.8rem;
+    font-size: 0.9rem;
+    font-weight: 600;
+  }
+
+  .react-calendar {
+    border: none;
+    background-color: transparent;
+
+    button {
+      border-radius: 10px;
+    }
+  }
+
+  .react-calendar__tile--now {
+    background-color: #006edc;
+    color: #fff;
+  }
+
+  .react-calendar__tile--now:enabled:hover,
+  .react-calendar__tile--now:enabled:focus {
+    background-color: #1087ff;
+  }
+
+  .react-calendar__tile--active {
+    background-color: #777;
+  }
+
+  .react-calendar__tile--active:enabled:hover,
+  .react-calendar__tile--active:enabled:focus {
+    background-color: #aaa;
+  }
+`;
+
+export const StyledTodo = styled.div`
+  margin: 1.8rem 0;
+
+  h1 {
+    margin-bottom: 0.8rem;
+    font-size: 0.9rem;
+    font-weight: 600;
+  }
+
+  ul {
+    li {
+      margin: 0.5rem 0;
+      font-size: 0.8rem;
+    }
+
+    li::before {
+      content: '-';
+      margin-right: 0.4rem;
+    }
+  }
+`;
+
+export const StyledQuickButtonWrapper = styled.div`
+  margin: 1rem 0;
+
+  h1 {
+    margin-bottom: 0.8rem;
+    font-size: 0.9rem;
+    font-weight: 600;
+  }
+
+  .button-wrapper {
+    position: relative;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+
+  button {
+    width: 40%;
+    margin: 0.3rem 0;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    border-radius: 10px;
+    background-color: #000;
+    font-size: 0.9rem;
+    color: #fff;
+    cursor: pointer;
+    z-index: 1;
+    transition: all 0.5s;
+
+    /* for ratio 1:1 box */
+    &::after {
+      content: '';
+      padding-bottom: 100%;
+      display: block;
+    }
+
+    /* for slide animation */
+    &::before {
+      content: '';
+      width: 0;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      border-radius: 9px;
+      background-color: #fff;
+      z-index: -1;
+      transition: all 0.5s;
+    }
+
+    &:hover {
+      color: #333;
+
+      &::before {
+        width: 100%;
+      }
+    }
+  }
 `;
--- src/components/organisms/QuickNavigation/QuickNavigation.tsx
+++ src/components/organisms/QuickNavigation/QuickNavigation.tsx
@@ -1,7 +1,50 @@
-import { StyledNavigation } from './QuickNavigation.styled';
+import { StyledNavigation, StyledCalendar, StyledTodo, StyledQuickButtonWrapper } from './QuickNavigation.styled';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const QuickNavigation: React.FC = () => {
-  return <StyledNavigation></StyledNavigation>;
+  const tempTodayTodo = ['김윤환 컨설턴트와 컨설팅 예약'];
+  const tempMonthTodo = [
+    '8일 김윤환 컨설턴트와 컨설팅',
+    '15일 자기소개서 제출하기',
+    '22일 면접 준비하기',
+    '29일 김윤환 컨설턴트와 컨설팅',
+  ];
+  return (
+    <StyledNavigation>
+      <StyledCalendar>
+        <h1>입시 캘린더</h1>
+        <Calendar calendarType="US" />
+      </StyledCalendar>
+      <StyledTodo>
+        <h1>오늘 할 일</h1>
+        <ul>
+          {tempTodayTodo.map(todo => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+      </StyledTodo>
+      <StyledTodo>
+        <h1>이번 달 할 일</h1>
+        <ul>
+          {tempMonthTodo.map(todo => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+      </StyledTodo>
+      <StyledQuickButtonWrapper>
+        <h1>바로가기</h1>
+        <div className="button-wrapper">
+          <button>일정 관리</button>
+          <button>결제 관리</button>
+          <button>공지사항</button>
+          <button>입시 뉴스</button>
+          <button>FAQ</button>
+          <button>FAQ</button>
+        </div>
+      </StyledQuickButtonWrapper>
+    </StyledNavigation>
+  );
 };
 
 export default QuickNavigation;
