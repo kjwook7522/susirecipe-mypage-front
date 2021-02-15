25ce3ed1385127708ee39d08097f152a47700984
kjwook7522 <kjwook7522@gmail.com>
2021-02-15 14:22:08 +0900

    Feat: create SNSad component

---
+++ src/components/organisms/SNSad/SNSad.styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const StyledSNSad = styled.div`
+  width: 100%;
+
+  p {
+    margin-bottom: 1.5rem;
+    color: #404040;
+  }
+
+  .sns-btn {
+    margin-right: 1.5rem;
+
+    svg {
+      margin-right: 0.5rem;
+      font-size: 1.4rem;
+    }
+  }
+`;
---
+++ src/components/organisms/SNSad/SNSad.tsx
@@ -0,0 +1,20 @@
+import { FlexButton } from 'components/atoms/Button/Button';
+import { StyledSNSad } from './SNSad.styled';
+import { RiKakaoTalkFill, RiInstagramLine } from 'react-icons/ri';
+
+const SNSad: React.FC = () => {
+  return (
+    <StyledSNSad>
+      <p>채널을 추가하면 유익하고 맞춤화된 입시 정보를 받아볼 수 있습니다.</p>
+      <FlexButton className="sns-btn" width="150px" height="50px" theme="kakao">
+        <RiKakaoTalkFill id="kakao" /> 채널 추가
+      </FlexButton>
+      <FlexButton className="sns-btn" width="150px" height="50px" theme="insta">
+        <RiInstagramLine id="instagram" />
+        채널 추가
+      </FlexButton>
+    </StyledSNSad>
+  );
+};
+
+export default SNSad;
