2eaa0435ec7fb954f2e62b26270b7b38072dda0b
kjwook7522 <kjwook7522@gmail.com>
2021-02-16 13:52:02 +0900

    Fix: App useeffect

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import { updateMonthTodo, updateTodayTodo } from 'actions/todoList';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
-  const tempTodayTodo = ['김윤환 컨설턴트와 컨설팅 예약'];
-  const tempMonthTodo = [
-    '8일 김윤환 컨설턴트와 컨설팅',
-    '15일 자기소개서 제출하기',
-    '22일 면접 준비하기',
-    '29일 김윤환 컨설턴트와 컨설팅',
-  ];
 
   useEffect(() => {
+    const tempTodayTodo = ['김윤환 컨설턴트와 컨설팅 예약'];
+    const tempMonthTodo = [
+      '8일 김윤환 컨설턴트와 컨설팅',
+      '15일 자기소개서 제출하기',
+      '22일 면접 준비하기',
+      '29일 김윤환 컨설턴트와 컨설팅',
+    ];
+
     dispatch(updateTodayTodo(tempTodayTodo));
     dispatch(updateMonthTodo(tempMonthTodo));
-  }, []);
+  }, [dispatch]);
   return <AppRouter />;
 };
 
