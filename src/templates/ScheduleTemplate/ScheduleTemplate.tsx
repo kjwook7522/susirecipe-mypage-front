d2276dd7b6299f882680fe18119a75f5b9bd4b7d
kjwook7522 <kjwook7522@gmail.com>
2021-02-16 18:59:52 +0900

    Fix: add confirm popup

--- src/templates/ScheduleTemplate/ScheduleTemplate.tsx
+++ src/templates/ScheduleTemplate/ScheduleTemplate.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'common/store';
 import FlexButton from 'components/atoms/FlexButton/FlexButton';
+import Input from 'components/atoms/Input/Input';
 import Calendar from 'components/molecules/Calendar/Calendar';
-import MenuPopup from 'components/molecules/MenuPopup/MenuPopup';
+import ConfirmPopup from 'components/molecules/ConfirmPopup/ConfirmPopup';
 import ScheduleTodoList from 'components/molecules/ScheduleTodoList/ScheduleTodoList';
+import styled from 'styled-components';
 import { useClickOutside } from 'hooks/useClickOutside';
 import {
   StyledScheduleTemplate,
@@ -12,27 +14,83 @@ import {
   StyledCalendarSection,
   StyledTodoSection,
 } from './ScheduleTemplate.styled';
+import { addMonthTodo } from 'actions/todoList';
+
+const StyledPopupContent = styled.div`
+  padding: 1.4em 2em;
+
+  h1 {
+    margin-bottom: 0.4rem;
+    color: #333;
+    font-size: 1rem;
+  }
+
+  .input-box {
+    margin-bottom: 1rem;
+  }
+`;
 
 const ScheduleTemplate: React.FC = () => {
-  const [activeMenu, setActiveMenu] = useState(false);
-  const plusBtnRef = useClickOutside(() => setActiveMenu(false));
+  const dispath = useDispatch();
+  const [activePopup, setActivePopup] = useState(false);
+  const [popupRef, plusBtnRef] = useClickOutside(() => setActivePopup(false));
+  const [popupInput, setPopupInput] = useState('');
 
-  const handleClick = () => {
-    setActiveMenu(true);
+  const handleAddButton = () => {
+    setActivePopup(true);
+  };
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setPopupInput(value);
+  };
+
+  const canclePopup = () => {
+    setActivePopup(false);
+  };
+
+  const confirmPopup = () => {
+    dispath(addMonthTodo(popupInput));
+    setPopupInput('');
+    setActivePopup(false);
   };
 
   const myTodoList = useSelector((state: RootState) => state.todoListReducer);
-  const scheduleMenu = ['일정 추가', '일정 수정'];
 
   return (
     <StyledScheduleTemplate>
       <StyledPageTitle>일정 관리</StyledPageTitle>
       <StyledCalendarSection>
         <Calendar />
-        <FlexButton className="plus-btn" width="30px" height="30px" clickRef={plusBtnRef} onClick={handleClick}>
+        <FlexButton className="plus-btn" width="30px" height="30px" clickRef={plusBtnRef} onClick={handleAddButton}>
           +
         </FlexButton>
-        <MenuPopup menuList={scheduleMenu} top="10px" right="0" active={activeMenu} />
+
+        <ConfirmPopup
+          top="10px"
+          right="0"
+          active={activePopup}
+          clickRef={popupRef}
+          cancleCallback={canclePopup}
+          confirmCallback={confirmPopup}
+        >
+          <StyledPopupContent>
+            <div className="input-box">
+              <h1>일정</h1>
+              <Input width="250px" backgroundColor="#f6f6f6" />
+            </div>
+            <div className="input-box">
+              <h1>제목</h1>
+              <Input
+                width="250px"
+                value={popupInput}
+                backgroundColor="#f6f6f6"
+                placeholder="일정 제목을 입력하세요"
+                onChange={handleInput}
+              />
+            </div>
+          </StyledPopupContent>
+        </ConfirmPopup>
       </StyledCalendarSection>
       <StyledTodoSection>
         <h1>- 이번 달 할일</h1>
