10abccfc2825b4db5ac3549f853b1ae2d477e3d8
kjwook7522 <kjwook7522@gmail.com>
2021-02-16 14:27:29 +0900

    Feat: implement schedule template

--- src/templates/ScheduleTemplate/ScheduleTemplate.styled.ts
+++ src/templates/ScheduleTemplate/ScheduleTemplate.styled.ts
@@ -1,3 +1,49 @@
 import styled from 'styled-components';
 
-export const StyledScheduleTemplate = styled.div``;
+export const StyledScheduleTemplate = styled.div`
+  padding: 2.5rem 4.5rem;
+`;
+
+export const StyledPageTitle = styled.div`
+  margin-bottom: 1.5rem;
+  font-weight: 600;
+  font-size: 1.25rem;
+`;
+
+export const StyledCalendarSection = styled.section`
+  margin-bottom: 3rem;
+  position: relative;
+
+  .react-calendar {
+    button {
+      border-radius: 10px;
+      font-size: 1rem;
+    }
+    .react-calendar__tile {
+      padding: 1.6em 0.5em;
+      box-sizing: border-box;
+    }
+
+    .react-calendar__month-view__days {
+    }
+  }
+
+  .plus-btn {
+    position: absolute;
+    top: -30px;
+    right: 7px;
+  }
+`;
+
+export const StyledTodoSection = styled.section`
+  padding: 2rem;
+  border: none;
+  border-radius: 10px;
+  background-color: #f6f6f6;
+
+  h1 {
+    margin-bottom: 0.8rem;
+    font-weight: 600;
+    font-size: 1rem;
+  }
+`;
--- src/templates/ScheduleTemplate/ScheduleTemplate.tsx
+++ src/templates/ScheduleTemplate/ScheduleTemplate.tsx
@@ -1,7 +1,47 @@
-import { StyledScheduleTemplate } from './ScheduleTemplate.styled';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from 'common/store';
+import { FlexButton } from 'components/atoms/Button/Button';
+import Calendar from 'components/molecules/Calendar/Calendar';
+import MenuPopup from 'components/molecules/MenuPopup/MenuPopup';
+import ScheduleTodoList from 'components/molecules/ScheduleTodoList/ScheduleTodoList';
+import { useClickOutside } from 'hooks/useClickOutside';
+import {
+  StyledScheduleTemplate,
+  StyledPageTitle,
+  StyledCalendarSection,
+  StyledTodoSection,
+} from './ScheduleTemplate.styled';
 
 const ScheduleTemplate: React.FC = () => {
-  return <StyledScheduleTemplate></StyledScheduleTemplate>;
+  const [activeMenu, setActiveMenu] = useState(false);
+  const plusBtnRef = useClickOutside(() => setActiveMenu(false));
+
+  const handleClick = () => {
+    setActiveMenu(true);
+  };
+
+  const myTodoList = useSelector((state: RootState) => state.todoListReducer);
+  const scheduleMenu = ['일정 추가', '일정 수정'];
+
+  return (
+    <StyledScheduleTemplate>
+      <StyledPageTitle>일정 관리</StyledPageTitle>
+      <StyledCalendarSection>
+        <Calendar />
+        <FlexButton className="plus-btn" width="30px" height="30px" clickRef={plusBtnRef} onClick={handleClick}>
+          +
+        </FlexButton>
+        <MenuPopup menuList={scheduleMenu} top="10px" right="0" active={activeMenu} />
+      </StyledCalendarSection>
+      <StyledTodoSection>
+        <h1>- 이번 달 할일</h1>
+        <ScheduleTodoList todoList={myTodoList.month} type="MONTH" />
+        <h1>- 금일 할 일</h1>
+        <ScheduleTodoList todoList={myTodoList.today} type="TODAY" />
+      </StyledTodoSection>
+    </StyledScheduleTemplate>
+  );
 };
 
 export default ScheduleTemplate;
