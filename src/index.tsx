6e112f00c5816534031c9a6d54b2c4586ed77645
kjwook7522 <kjwook7522@gmail.com>
2021-02-16 12:34:01 +0900

    Feat: create redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './common/reset.css';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store } from 'common/store';
 import App from 'App';
+import 'common/reset.css';
+import 'index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
