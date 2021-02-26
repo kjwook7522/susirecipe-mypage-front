4405a57da5d20b8c9b11942c17ca3df52beee43a
kjwook7522 <kjwook7522@gmail.com>
2021-02-26 14:26:59 +0900

    Fix: clean up FAQ template

--- src/templates/FAQTemplate/FAQTemplate.tsx
+++ src/templates/FAQTemplate/FAQTemplate.tsx
@@ -1,26 +1,13 @@
-import { StyledFAQTemplate, StyledPageTitle, StyledFAQBoardSection, StyledSearchBar } from './FAQTemplate.styled';
-import { FaSearch } from 'react-icons/fa';
+import FAQBoard from 'components/molecules/FAQBoard/FAQBoard';
+import { StyledFAQTemplate, StyledPageTitle, StyledFAQBoardSection } from './FAQTemplate.styled';
 
 const FAQTemplate: React.FC = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  }
   return (
     <StyledFAQTemplate>
       <StyledPageTitle>FAQ</StyledPageTitle>
+
       <StyledFAQBoardSection>
-        <StyledSearchBar onSubmit={handleSubmit}>
-          <select>
-            <option value="">제목</option>
-            <option value="">작성자</option>
-            <option value="">내용</option>
-          </select>
-          <input type="text" placeholder="검색" />
-          <button>
-            <FaSearch />
-          </button>
-        </StyledSearchBar>
-        123
+        <FAQBoard />
       </StyledFAQBoardSection>
     </StyledFAQTemplate>
   );
