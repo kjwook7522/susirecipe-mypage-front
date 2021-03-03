eb2f443d6d85923634642930f44ed3d9d8d50564
kjwook7522 <kjwook7522@gmail.com>
2021-03-03 16:08:49 +0900

    Fix: add props of progress dot

--- src/components/atoms/ProgressDot/ProgressDot.tsx
+++ src/components/atoms/ProgressDot/ProgressDot.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyledProgressDot } from './ProgressDot.styled';
 
 interface Props {
+  idx: number;
   times: number;
+  handlePopup: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const ProgressDot: React.FC<Props> = ({ times }) => {
+const ProgressDot: React.FC<Props> = ({ idx, times, handlePopup }) => {
   return (
-    <StyledProgressDot>
+    <StyledProgressDot onClick={() => handlePopup(idx)}>
       <span>{times}</span>
     </StyledProgressDot>
   );
