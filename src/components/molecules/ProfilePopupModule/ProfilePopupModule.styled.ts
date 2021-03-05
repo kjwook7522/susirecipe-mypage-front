ec99d1499501caba1c86e0ce9aa2eb5e1233cadd
kjwook7522 <kjwook7522@gmail.com>
2021-03-05 18:20:36 +0900

    Feat: create profile popup module to component

---
+++ src/components/molecules/ProfilePopupModule/ProfilePopupModule.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const StyledProfilePopupModule = styled.div`
+  position: relative;
+`;
+
+export const StyledUsername = styled.span`
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
---
+++ src/components/molecules/ProfilePopupModule/ProfilePopupModule.tsx
@@ -0,0 +1,28 @@
+import { useClickOutside } from 'hooks/useClickOutside';
+import { useCallback, useState } from 'react';
+import ProfilePopup from '../ProfilePopup/ProfilePopup';
+import { StyledProfilePopupModule, StyledUsername } from './ProfilePopupModule.styled';
+
+interface Props {
+  userName: string;
+}
+
+const ProfilePopupModule: React.FC<Props> = ({ userName }) => {
+  const [isPopup, setIsPopup] = useState(false);
+  const [popupRef, buttonRef] = useClickOutside(() => setIsPopup(false));
+
+  const handlePopup = useCallback(() => {
+    setIsPopup(prev => !prev);
+  }, []);
+
+  return (
+    <StyledProfilePopupModule>
+      <StyledUsername ref={buttonRef} onClick={handlePopup}>
+        {userName}
+      </StyledUsername>
+      {isPopup && <ProfilePopup clickRef={popupRef} top="70px" left="-80px" cancleCallback={handlePopup} />}
+    </StyledProfilePopupModule>
+  );
+};
+
+export default ProfilePopupModule;
