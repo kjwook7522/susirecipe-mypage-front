2009fc57c592ed66c9fb4be13864f91a60a137c1
kjwook7522 <kjwook7522@gmail.com>
2021-03-05 19:05:05 +0900

    Fix: add react memo to icon button

--- src/components/atoms/IconButton/IconButton.tsx
+++ src/components/atoms/IconButton/IconButton.tsx
@@ -1,13 +1,19 @@
+import React from 'react';
 import { StyledIconButton } from './IconButton.styled';
 
 interface Props {
   color?: string;
   clickRef?: React.RefObject<any>;
+  children?: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLSpanElement>;
 }
 
 const IconButton: React.FC<Props> = ({ children, clickRef, ...props }) => {
-  return <StyledIconButton ref={clickRef} {...props}>{children}</StyledIconButton>;
+  return (
+    <StyledIconButton ref={clickRef} {...props}>
+      {children}
+    </StyledIconButton>
+  );
 };
 
-export default IconButton;
+export default React.memo(IconButton);
